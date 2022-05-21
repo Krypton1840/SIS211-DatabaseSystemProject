acd3e87850f72ab5bb9975feaf80a2aae504d537
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-05-21 22:07:53 +0200

    Linked menu page with signup pages (go to and go back) [Without termination of any of them]

--- gui.py
+++ gui.py
@@ -12,50 +12,58 @@ ASSETS_PATH = OUTPUT_PATH / Path("./assets")
     
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
-
-window = Tk()
-window.title("Waddy Menu")
-window.geometry("1048x768")
-window.configure(bg = "#FFFFFF")
-
-
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 768,
-    width = 1048,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-
-def goClientSignUp():
-    window.destroy()
-    import guiclientsignup
-def goAdminSignUp():
-    window.destroy()
-    import guiadminsignup
-
-#-----------------------------------------Choose [ Button ]-----------------------------------------
-
-buttonFont = font.Font(family='Segoe UI', size=18, weight='bold')
-
-signup_client_button = Button(text='Sign up client',bg='#4D47C3',fg='#ffffff',font=buttonFont,borderwidth=0,highlightthickness=0,
-                       command=goClientSignUp,
-                       relief="flat"
-                )
-
-signup_client_button.place(x=319.0,y=384.0,width=409.0,height=42.0)
-
-signup_admin_button = Button(text='Sign up admin',bg='#4D47C3',fg='#ffffff',font=buttonFont,borderwidth=0,highlightthickness=0,
-                       command=goAdminSignUp,
-                       relief="flat"
-                )
-
-signup_admin_button.place(x=319.0,y=300.0,width=409.0,height=42.0)
+class MenuPage:
+    def __init__(self):
+        
+        window = Tk()
+        window.title("Waddy Menu")
+        window.geometry("1048x768")
+        window.configure(bg = "#FFFFFF")
 
 
-window.resizable(False, False)
-window.mainloop()
+        canvas = Canvas(
+            window,
+            bg = "#FFFFFF",
+            height = 768,
+            width = 1048,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+
+        canvas.place(x = 0, y = 0)
+
+        def goClientSignUp():
+            window.destroy()
+            import guiclientsignup
+            guiclientsignup.ClientSignUpPage()
+        def goAdminSignUp():
+            window.destroy()
+            import guiadminsignup
+            guiadminsignup.AdminSignUpPage()
+
+        #-----------------------------------------Choose [ Button ]-----------------------------------------
+
+        buttonFont = font.Font(family='Segoe UI', size=18, weight='bold')
+
+        signup_client_button = Button(text='Sign up client',bg='#4D47C3',fg='#ffffff',font=buttonFont,borderwidth=0,highlightthickness=0,
+                               command=goClientSignUp,
+                               relief="flat"
+                        )
+
+        signup_client_button.place(x=319.0,y=384.0,width=409.0,height=42.0)
+
+        signup_admin_button = Button(text='Sign up admin',bg='#4D47C3',fg='#ffffff',font=buttonFont,borderwidth=0,highlightthickness=0,
+                               command=goAdminSignUp,
+                               relief="flat"
+                        )
+
+        signup_admin_button.place(x=319.0,y=300.0,width=409.0,height=42.0)
+
+
+        window.resizable(False, False)
+        window.mainloop()
+
+if __name__ == "__main__":
+    app = MenuPage()
+    #app.mainloop()
--- guiclientsignup.py
+++ guiclientsignup.py
@@ -13,120 +13,123 @@ ASSETS_PATH = OUTPUT_PATH / Path("./assets")
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
-
-window = Tk()
-window.title("Waddy Client SignUp")
-window.geometry("1048x768")
-window.configure(bg = "#FFFFFF")
-
-
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 768,
-    width = 1048,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
+class ClientSignUpPage:
+    def __init__(self):
+    
+        window = Tk()
+        window.title("Waddy Client SignUp")
+        window.geometry("1048x768")
+        window.configure(bg = "#FFFFFF")
 
 
-canvas.create_text(52.0,42.0,anchor="nw",text="Waddy",fill="#000000",font=("Segoe UI", 20 * -1))
+        canvas = Canvas(
+            window,
+            bg = "#FFFFFF",
+            height = 768,
+            width = 1048,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
 
-canvas.create_text(129.0,284.0,anchor="nw",text="Sign Up to ",fill="#000000",font=("Segoe UI", 50 * -1, 'bold'))
-
-canvas.create_text(129.0,354.0,anchor="nw",text="Waddy",fill="#000000",font=("Segoe UI", 35 * -1))
-
-
-
-#-----------------------------------------First Name [ Entry ]-----------------------------------------
-
-canvas.create_text(541.0,181.0,anchor="nw",text="First Name",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
-
-first_name_entry_image = PhotoImage( file=relative_to_assets("first_name_entry_image.png"))
-
-first_name_entry_bg = canvas.create_image(735.5,223.5,image=first_name_entry_image)
-
-first_name_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
-
-first_name_entry.place(x=539.0,y=202.0,width=393.0,height=41.0)
-
-#-----------------------------------------Last Name [ Entry ]-----------------------------------------
-
-canvas.create_text(541.0,249.0,anchor="nw",text="Last Name",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
-
-last_name_entry_image = PhotoImage(file=relative_to_assets("last_name_entry_image.png"))
-
-last_name_entry_bg = canvas.create_image(735.5,292.0,image=last_name_entry_image)
-
-last_name_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
-
-last_name_entry.place(x=539.0,y=270.0,width=393.0,height=42.0)
-
-#-----------------------------------------Email [ Entry ]-----------------------------------------
-
-canvas.create_text(541.0,317.0,anchor="nw",text="Email",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
-
-email_entry_image = PhotoImage(file=relative_to_assets("email_entry_image.png"))
-
-email_entry_bg = canvas.create_image(735.5,359.0,image=email_entry_image)
-
-email_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
-
-email_entry.place(x=539.0,y=337.0,width=393.0,height=42.0)
-
-#-----------------------------------------Password [ Entry ]-----------------------------------------
-
-canvas.create_text(541.0,385.0,anchor="nw",text="Password",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
-
-password_entry_image = PhotoImage(file=relative_to_assets("password_entry_image.png"))
-
-password_entry_bg = canvas.create_image(735.5,427.5,image=password_entry_image)
-
-password_entry = Entry(bd=0,show="●",bg="#EFEFFF",highlightthickness=0)
-
-password_entry.place(x=539.0,y=406.0,width=393.0,height=41.0)
-
-#-----------------------------------------Telephone Number [ Entry ]-----------------------------------------
-
-canvas.create_text(541.0,453.0,anchor="nw",text="Telephone Number",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
-
-telephone_entry_image = PhotoImage(file=relative_to_assets("telephone_entry_image.png"))
-
-telephone_entry_bg = canvas.create_image(735.5,495.5,image=telephone_entry_image)
-
-telephone_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
-
-telephone_entry.place(x=539.0,y=473.0,width=393.0,height=43.0)
+        canvas.place(x = 0, y = 0)
 
 
-#-----------------------------------------SignUp [ Button ]-----------------------------------------
+        canvas.create_text(52.0,42.0,anchor="nw",text="Waddy",fill="#000000",font=("Segoe UI", 20 * -1))
 
-signup_button_image = PhotoImage(file=relative_to_assets("signup_button_image.png"))
+        canvas.create_text(129.0,284.0,anchor="nw",text="Sign Up to ",fill="#000000",font=("Segoe UI", 50 * -1, 'bold'))
 
-signup_button = Button(image=signup_button_image,borderwidth=0,highlightthickness=0,
-                       command=lambda:[signUpClient(first_name_entry,last_name_entry,email_entry,password_entry,telephone_entry)],
-                       relief="flat"
-                )
-
-signup_button.place(x=531.0,y=555.0,width=409.0,height=42.0)
-
-#-----------------------------Go Back [Button]------------------------------------
-def goBackToMenu():
-    window.destroy()
-    import gui
-buttonFont = font.Font(family='Segoe UI', size=14, weight='bold')
-go_back_button = Button(text='Go back to menu',bg='#ffffff',fg='#4D47C3',font=buttonFont,borderwidth=0,highlightthickness=0,
-                       command=goBackToMenu,
-                       relief="flat"
-                )
-
-go_back_button.place(x=89.0,y=611,width=160.0,height=42.0)
+        canvas.create_text(129.0,354.0,anchor="nw",text="Waddy",fill="#000000",font=("Segoe UI", 35 * -1))
 
 
 
+        #-----------------------------------------First Name [ Entry ]-----------------------------------------
 
-window.resizable(False, False)
-window.mainloop()
+        canvas.create_text(541.0,181.0,anchor="nw",text="First Name",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
+
+        first_name_entry_image = PhotoImage( file=relative_to_assets("first_name_entry_image.png"))
+
+        first_name_entry_bg = canvas.create_image(735.5,223.5,image=first_name_entry_image)
+
+        first_name_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
+
+        first_name_entry.place(x=539.0,y=202.0,width=393.0,height=41.0)
+
+        #-----------------------------------------Last Name [ Entry ]-----------------------------------------
+
+        canvas.create_text(541.0,249.0,anchor="nw",text="Last Name",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
+
+        last_name_entry_image = PhotoImage(file=relative_to_assets("last_name_entry_image.png"))
+
+        last_name_entry_bg = canvas.create_image(735.5,292.0,image=last_name_entry_image)
+
+        last_name_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
+
+        last_name_entry.place(x=539.0,y=270.0,width=393.0,height=42.0)
+
+        #-----------------------------------------Email [ Entry ]-----------------------------------------
+
+        canvas.create_text(541.0,317.0,anchor="nw",text="Email",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
+
+        email_entry_image = PhotoImage(file=relative_to_assets("email_entry_image.png"))
+
+        email_entry_bg = canvas.create_image(735.5,359.0,image=email_entry_image)
+
+        email_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
+
+        email_entry.place(x=539.0,y=337.0,width=393.0,height=42.0)
+
+        #-----------------------------------------Password [ Entry ]-----------------------------------------
+
+        canvas.create_text(541.0,385.0,anchor="nw",text="Password",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
+
+        password_entry_image = PhotoImage(file=relative_to_assets("password_entry_image.png"))
+
+        password_entry_bg = canvas.create_image(735.5,427.5,image=password_entry_image)
+
+        password_entry = Entry(bd=0,show="●",bg="#EFEFFF",highlightthickness=0)
+
+        password_entry.place(x=539.0,y=406.0,width=393.0,height=41.0)
+
+        #-----------------------------------------Telephone Number [ Entry ]-----------------------------------------
+
+        canvas.create_text(541.0,453.0,anchor="nw",text="Telephone Number",fill="#A7A2FF",font=("Segoe UI", 15 * -1))
+
+        telephone_entry_image = PhotoImage(file=relative_to_assets("telephone_entry_image.png"))
+
+        telephone_entry_bg = canvas.create_image(735.5,495.5,image=telephone_entry_image)
+
+        telephone_entry = Entry(bd=0,bg="#EFEFFF",highlightthickness=0)
+
+        telephone_entry.place(x=539.0,y=473.0,width=393.0,height=43.0)
+
+
+        #-----------------------------------------SignUp [ Button ]-----------------------------------------
+
+        signup_button_image = PhotoImage(file=relative_to_assets("signup_button_image.png"))
+
+        signup_button = Button(image=signup_button_image,borderwidth=0,highlightthickness=0,
+                               command=lambda:[signUpClient(first_name_entry,last_name_entry,email_entry,password_entry,telephone_entry)],
+                               relief="flat"
+                        )
+
+        signup_button.place(x=531.0,y=555.0,width=409.0,height=42.0)
+
+        #-----------------------------Go Back [Button]------------------------------------
+        def goBackToMenu():
+            window.destroy()
+            import gui
+            gui.MenuPage()
+        buttonFont = font.Font(family='Segoe UI', size=14, weight='bold')
+        go_back_button = Button(text='Go back to menu',bg='#ffffff',fg='#4D47C3',font=buttonFont,borderwidth=0,highlightthickness=0,
+                               command=goBackToMenu,
+                               relief="flat"
+                        )
+
+        go_back_button.place(x=89.0,y=611,width=160.0,height=42.0)
+
+
+
+
+        window.resizable(False, False)
+        window.mainloop()
